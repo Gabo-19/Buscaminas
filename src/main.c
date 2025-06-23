417eef9ba965b8fb52a7b545262533bdd066723d
Juanpa MR <juanpamr@Ubuntu.myguest.virtualbox.org>
2025-06-22 21:31:21 -0600

    Actualización del proyecto, segundo avance

--- src/main.c
+++ src/main.c
@@ -1,27 +1,28 @@
 #include "raylib.h"
-#include "raymath.h"
 #include "juego.h"
-    #define SCREEN_WIDTH 600
-    #define SCREEN_HEIGHT 600
+
+#define SCREEN_WIDTH 60
+#define SCREEN_HEIGHT 600
 
 
-int main(void) //aqui abre una ventana nada mas por ahora falta hacer las funciones de inicio y renderizado
+int main() //aqui abre una ventana nada mas, por ahora falta hacer las funciones de inicio y renderizado
 {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Buscaminas proyecto");
     SetTargetFPS(60);
-    Iniciarjuego();
+    GameStartUp();
 
     while (!WindowShouldClose())
     {
-        Iniciarjuego();
+        GameUpdate();
         BeginDrawing();
         ClearBackground(DARKGREEN);
 
-        Render();
+        GameRender();
         EndDrawing();
     }
 
-    Apagarjuego();
+    GameShutdown();
     CloseWindow();
-    return 0
+
+    return 0;
 }
