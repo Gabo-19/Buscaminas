be0f3191a98c8a89ded4c0d0ce67cdcd368988a3
Juanpa MR <juanpamr@Ubuntu.myguest.virtualbox.org>
2025-06-28 20:38:21 -0600

    Codigo completo en sus respectivos archivos

--- src/main.c
+++ src/main.c
@@ -1,15 +1,12 @@
 #include "raylib.h"
+#include "raymath.h"
 #include "juego.h"
 
-#define SCREEN_WIDTH 60
-#define SCREEN_HEIGHT 600
+int main() {
 
-
-int main() //aqui abre una ventana nada mas, por ahora falta hacer las funciones de inicio y renderizado
-{
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Buscaminas proyecto");
     SetTargetFPS(60);
-    GameStartUp();
+    GameStartup();
 
     while (!WindowShouldClose())
     {
